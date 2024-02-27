8b00232df098bd65f90882760aa3444d2557dfeb
Papa Lisa <ishakgang@catdog.cc>
2024-02-27 12:25:02 +0300

    ProjectInitial

---
+++ src/CursachIsp-21k/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CursachIsp_21k;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
